3028e463b0f4f80b83ab1e45fd4f82333bb5d9c3
oldman07 <backagain010@gmail.com>
2021-08-19 12:40:00 +0500

    laravel+vue

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>hi from vue 3</h1>
+    <Hello />
+</template>
+
+<script>
+
+import Hello from './components/Hello.vue';
+
+export default {
+    name: 'App',
+    components: {Hello}
+}
+</script>
